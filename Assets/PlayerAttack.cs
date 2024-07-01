88f37e4798fb8e5ee464c23abc04ed3c17549ba5
dunglt <dung2002ss@gmail.com>
2024-07-01 17:44:12 +0700

    Add range increasing for player

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -9,7 +9,17 @@ public class PlayerAttack : MonoBehaviour
     private PlayerStateManager stateManager;
 
     [SerializeField]
+    private GameObject groundCircle;
+
+    [SerializeField]
     private float radius = 5f;
+
+    [SerializeField]
+    private GameObject enemy;
+
+    [SerializeField]
+    public bool hasEnemy = false;
+
     private void Reset()
     {
         this.LoadComponent();
@@ -18,22 +28,46 @@ public class PlayerAttack : MonoBehaviour
     private void LoadComponent()
     {
         stateManager = this.transform.parent.GetComponent<PlayerStateManager>();
+        groundCircle = GameObject.Find("GroundCircle").gameObject;
     }
 
     private void FixedUpdate()
     {
-        FindEnemies(this.transform.position, radius);
+        enemy = FindEnemies(this.transform.position, radius);
+
+        if (hasEnemy )
+        {
+            Debug.Log("enemy: " + enemy.name);
+        }
+
     }
 
-    private void FindEnemies(Vector3 center, float radius)
+    private GameObject FindEnemies(Vector3 center, float radius)
     {
         Collider[] hitColliders = Physics.OverlapSphere(center, radius);
         foreach (var hitCollider in hitColliders)
         {
             if (hitCollider.CompareTag("Enemy"))
             {
-                Debug.Log("FoundEnemy");
+                hasEnemy = true;
+                return hitCollider.gameObject;
             }
         }
+
+        hasEnemy = false;
+        return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position to visualize the detection radius
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void IncreaseRange()
+    {
+        radius *= 1.1f;
+        groundCircle.transform.localScale = new Vector3(groundCircle.transform.localScale.x * 1.1f, groundCircle.transform.localScale.y * 1.1f, groundCircle.transform.localScale.z);
     }
 }
