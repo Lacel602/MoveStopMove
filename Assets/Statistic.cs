507f460ba251127106cd7b746b76f8eb256d9b7a
dunglt <dung2002ss@gmail.com>
2024-07-08 10:30:34 +0700

    Smooth size increase for player and backward camera

--- Assets/Statistic.cs
+++ Assets/Statistic.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Statistic : MonoBehaviour
@@ -39,7 +40,10 @@ public class Statistic : MonoBehaviour
             level++;
 
             //Increase size of player
-            this.transform.localScale *= ConstantStat.increaseSize;
+
+            Vector3 newScale = this.transform.localScale * ConstantStat.increaseSize;
+            StartCoroutine(ChangeVectorOverTime(this.transform.localScale, newScale, 0.2f));
+            
 
             if (this.CompareTag("Player"))
             {
@@ -49,5 +53,29 @@ public class Statistic : MonoBehaviour
 
         //Change TMP score text
         scroreTMP.text = level.ToString();
+    }
+
+    private IEnumerator ChangeVectorOverTime(Vector3 start, Vector3 destination, float t)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < t)
+        {
+            // Interpolate between the two vectors
+            Vector3 currentVector = Vector3.Lerp(start, destination, elapsedTime / t);
+
+            // Update the value of the vector (this could be a position, a direction, etc.)
+            // For example, if you want to change the position of the GameObject:
+            this.transform.localScale = currentVector;
+
+            // Increment the elapsed time
+            elapsedTime += Time.deltaTime;
+
+            // Yield execution until the next frame
+            yield return null;
+        }
+
+        // Ensure the final value is set to the target vector
+        this.transform.localScale = destination;
     }
 }
